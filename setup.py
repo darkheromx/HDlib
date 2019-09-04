4889c455cebf0fa8856ca2313054da0fa5399cba
darkheromx <chadchai.ping@hotmail.com>
2019-09-04 18:39:54 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='eculib',
+    version='1.0.9',
+    description='A library for K-line based ECU communication',
+    url='https://github.com/darkheromx/HDlib',
+    author='Ryan M. Hope',
+    author_email='chadchai.ping.cs@hotmail.com',
+    license='GPL-3',
+    packages=['eculib'],
+    entry_points={
+        'console_scripts': ['eculib=eculib.__main__:Main'],
+    },
+    install_requires=['pylibftdi','pydispatcher'],
+)
